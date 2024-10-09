996ca40ba540302e6acc96b63ed8a05d987f0267
nsavva38 <nsavva38@gmail.com>
2024-10-09 12:24:37 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+alert(`You have received this message because you have been chosen to open an important vault. 
+Here is the secret combination:`);
+
+const numb1 = 10
+const numb2 = 40
+const numb3 = 39
+
+lockNumb1 = numb1 + numb2 + numb3
+lockNumb2 = numb3 - numb1 - numb2
+lockNumb3 =  numb3 * (numb2 / numb1)
+
+alert(`${lockNumb1}, ${lockNumb2}, ${lockNumb3} `);
