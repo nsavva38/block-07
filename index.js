11520b6980c49bc9430ec223aa33918f8d763f08
nsavva38 <nsavva38@gmail.com>
2024-10-09 12:59:36 -0400

    made the secret combo become 10-40-39

--- index.js
+++ index.js
@@ -6,15 +6,16 @@
 //    - do this three times
 // display to user the combination of the three variables ( non constants )
 
+// const numb1 = 10;
+// const numb2 = 40;
+// const numb3 = 39;
+
 alert(`You have received this message because you have been chosen to open an important vault. 
 Here is the secret combination:`);
 
-const numb1 = 10;
-const numb2 = 40;
-const numb3 = 39;
 
-lockNumb1 = numb1 + numb2 + numb3;
-lockNumb2 = numb3 - numb1 - numb2;
-lockNumb3 =  numb3 * (numb2 / numb1);
+lockNumb1 = 1 + 5 + 4;
+lockNumb2 = 10 * 2 * 2;
+lockNumb3 =  (80 / 2) - 1;
 
-alert(`${lockNumb1}, ${lockNumb2}, ${lockNumb3}`);
+alert(`${lockNumb1} - ${lockNumb2} - ${lockNumb3}`);
