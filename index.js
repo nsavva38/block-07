920fc264e8c53ada968d5251e2609a5dbb53e7b6
nsavva38 <nsavva38@gmail.com>
2024-10-09 13:23:23 -0400

    changed let to const

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-
-
 // tell user about vault
 // create constant variables for given numbers
 // create a variable that uses constants and arithmatic to create new value
 //    - do this three times
 // display to user the combination of the three variables ( non constants )
 
-// const numb1 = 10;
-// const numb2 = 40;
-// const numb3 = 39;
+// const numb1 10;
+// const numb2 40;
+// const numb3 39;
 
 alert(`You have received this message because you have been chosen to open an important vault. 
 Here is the secret combination:`);
 
 
-let lockNumb1 = 1 + 5 + 4;
-let lockNumb2 = 10 * 2 * 2;
-let lockNumb3 =  (80 / 2) - 1;
+const lockNumb1 = 1 + 5 + 4;
+const lockNumb2 = 10 * 2 * 2;
+const lockNumb3 =  (80 / 2) - 1;
 
 alert(`${lockNumb1} - ${lockNumb2} - ${lockNumb3}`);
